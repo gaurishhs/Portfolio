da2eb6bee0799c66aa5daba595e74cdfcbecbdf7
Gaurish Sethia <gaurishsethia@yahoo.com>
2023-10-02 20:10:14 +0530

    fix ts issue

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,3 +1,4 @@
+//@ts-nocheck
 import type { MDXComponents } from "mdx/types";
 import { Image } from "@/app/components/image";
 import { Pre } from "@/app/components/pre";
@@ -11,7 +12,6 @@ export function useMDXComponents(components: {
         // Allows customizing built-in components, e.g. to add styling.
         // h1: ({ children }) => <h1 style={{ fontSize: "100px" }}>{children}</h1>,
         ...components,
-        // @ts-ignore
         img: Image,
         pre: Pre,
         code: Code
