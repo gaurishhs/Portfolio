c05f1973eb8a03ebf06526ae93e9d6833b21fb80
Gaurish Sethia <gaurishsethia@yahoo.com>
2023-10-20 11:08:33 +0530

    Licensing

--- app/components/pre.tsx
+++ app/components/pre.tsx
@@ -2,14 +2,17 @@ import clsx from "clsx";
 export const Pre = ({
     children,
     scroll = true,
+    ...props
 }: {
     children: React.ReactNode;
     scroll?: boolean;
+    [key: string]: any;
 }) => (
     <div className="my-6">
         <pre
+            {...props}
             className={clsx(
-                "p-4 text-sm",
+                "p-4 text-sm dark:data-[theme='light']:hidden dark:data-[theme='dark']:block data-[theme='dark']:hidden",
                 scroll
                     ? "overflow-scroll"
                     : "whitespace-pre-wrap break-all overflow-hidden"
